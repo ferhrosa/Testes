6dd01aa9a9f64e406199b0568b370f32b345247e
Fernando Henrique Rosa <ferhrosa@gmail.com>
2018-02-16 19:45:06 -0200

    Tutorial Basic Platformer de Unity

---
+++ Unity/TutorialBasicPlatformer/Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+}
---
+++ Unity/TutorialBasicPlatformer/Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour
+{
+
+    public float fallDelay;
+
+    private Rigidbody2D rb2d;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", fallDelay);
+        }
+    }
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+        //rb2d.bodyType = RigidbodyType2D.Dynamic;
+    }
+}
